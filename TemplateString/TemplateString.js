f68f494853e682fd5895dfe8f0073edc5d8a3205
thanhtan45 <tandtse161266@fpt.edu.vn>
2022-05-21 20:33:53 +0700

    bai 5

---
+++ TemplateString/TemplateString.js
@@ -0,0 +1,3 @@
+console.log(`Hello world From my mom`)
+
+console.log(`Welcome back captain ${4} ${5} ${9} `)
--- Variables/variables.js
+++ Variables/variables.js
@@ -2,9 +2,15 @@ let x = 1,
     y = 2,
     z = 3;
 
+let a = 'a',
+    b = '4';
+
 console.log(x + y + 'a');
 //cong chuoi tu trai sang phai 1+2+a = 3a
 console.log('a' + x + y);
 //luu y khi co chu truoc 1 so thi se la a + 1 + 2 = a1 + 2 = a12
+
+console.log(x / a);
+console.log(x / b);
 
 console.log("Testing String ", 'x = ', x, 'z = ', z, 'y = ', y);
