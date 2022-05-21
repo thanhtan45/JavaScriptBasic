a2aa1b53267ccc7c834d9eea428596df67731740
thanhtan45 <tandtse161266@fpt.edu.vn>
2022-05-21 16:43:20 +0700

    bai 4

---
+++ Variables/variables.js
@@ -0,0 +1,10 @@
+let x = 1,
+    y = 2,
+    z = 3;
+
+console.log(x + y + 'a');
+//cong chuoi tu trai sang phai 1+2+a = 3a
+console.log('a' + x + y);
+//luu y khi co chu truoc 1 so thi se la a + 1 + 2 = a1 + 2 = a12
+
+console.log("Testing String ", 'x = ', x, 'z = ', z, 'y = ', y);
