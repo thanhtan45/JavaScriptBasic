1ac9bf50d0dc736dc7369310d8eafd7f51d54392
thanhtan45 <tandtse161266@fpt.edu.vn>
2022-05-24 21:16:18 +0700

    Array

---
+++ CodeJS/Arrays/Arrays.js
@@ -0,0 +1,10 @@
+console.log('Welcome Back Captain');
+let a = {
+    age: '12',
+    address: 'HCM'
+};
+
+//arr = [item1, item2, item3, item4,...]
+let b = [a, 'Dung', 'Le', 'Quang'];
+console.log(b)
+console.log(b[0].age)
