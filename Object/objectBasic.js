76e2e3f3b9061bdb0f025df201163722620e4188
thanhtan45 <tandtse161266@fpt.edu.vn>
2022-05-24 21:01:50 +0700

    objectbasic

---
+++ Object/objectBasic.js
@@ -0,0 +1,9 @@
+console.log(`Welcome back captain`);
+// khi khai báo object thì cần lưu ý với key : value
+// ví dụ key: value 
+let AnhA = {
+    age: '12',
+    address: '12/43 phuong trung'
+};
+//đây là key và value
+console.log(`My age: `, AnhA.age)
--- TemplateString/TemplateString.js
+++ TemplateString/TemplateString.js
@@ -1,3 +1,12 @@
 console.log(`Hello world From my mom`)
 
+//cách làm thông minh sử dụng `` thay vì "" ''
+
 console.log(`Welcome back captain ${4} ${5} ${9} `)
+
+let i = 0,
+    a = 0;
+for (i = 11; i < 21; i++) {
+    a = i + i;
+}
+console.log(a)
