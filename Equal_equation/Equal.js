0d398cea20ffde7ab1323e8c11b99c7b96bc94ff
thanhtan45 <tandtse161266@fpt.edu.vn>
2022-06-01 20:01:27 +0700

    Equal_Equation

---
+++ Equal_equation/Equal.js
@@ -0,0 +1,17 @@
+console.log('Hello world from HTML')
+let a = 5;
+let b = 5;
+
+console.log(`compare a and b `, a, b, `Result : `, a == b);
+
+//lưu ý khi sài === là kiểu so sánh giữ type, value 
+//lưu ý khi sài == là kiểu so sánh value
+
+// Empty, null, undefined
+
+// undefined
+let c;
+// null
+let e = null;
+// empty
+let f = "";
